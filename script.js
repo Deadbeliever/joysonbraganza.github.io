5ab833334e2ae0b68a65b5498f368ad6eec64206
Joyson Braganza <89176630+Deadbeliever@users.noreply.github.com>
2026-03-03 21:21:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-// Smooth Section Scrolling
+// Smooth scroll
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener("click", function (e) {
         e.preventDefault();
         document.querySelector(this.getAttribute("href"))
             .scrollIntoView({ behavior: "smooth" });
     });
+});
+
+// Fade-in on scroll
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+    threshold: 0.2
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) return;
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
 });
